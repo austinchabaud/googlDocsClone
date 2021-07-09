399d6ea7b7fc9e4a89cb366a5cc2a23f56fdb23e
Austin Chabaud <austinc1023@yahoo.com>
2021-07-08 22:17:40 -0500

    added documentId

--- src/TextEditor.js
+++ src/TextEditor.js
@@ -1,69 +1,87 @@
-import {useCallback, useEffect, useState} from 'react'
-import Quill from "quill"
-import "quill/dist/quill.snow.css"
-import {io} from "socket.io-client"
+import { useCallback, useEffect, useState } from 'react';
+import Quill from 'quill';
+import 'quill/dist/quill.snow.css';
+import { io } from 'socket.io-client';
+import { useParams } from 'react-router-dom';
+
 const TOOLBAR_OPTIONS = [
-    [{header: [1,2,3,4,5,6, false]}],
-    [{font: []}],
-    [{list: "ordered"}, {list: "bullet"}],
-    ["bold", "italic", "underline"],
-    [{color: []}, {background: []}],
-    [{script: "sub"}, {script:"super"}],
-    [{align: []}],
-    ["image", "blockquote", "code-block"],
-    ["clean"]
-]
+	[{ header: [1, 2, 3, 4, 5, 6, false] }],
+	[{ font: [] }],
+	[{ list: 'ordered' }, { list: 'bullet' }],
+	['bold', 'italic', 'underline'],
+	[{ color: [] }, { background: [] }],
+	[{ script: 'sub' }, { script: 'super' }],
+	[{ align: [] }],
+	['image', 'blockquote', 'code-block'],
+	['clean'],
+];
 
 export default function TextEditor() {
-    const [socket, setSocket] = useState()
-    const [quill, setQuill] = useState()
+	const { id: documentId } = useParams();
+	const [socket, setSocket] = useState();
+	const [quill, setQuill] = useState();
+	console.log(documentId);
 
-    useEffect(() =>{
-      const s = io("http://localhost:3001")
-      setSocket(s)
+	useEffect(() => {
+		const s = io('http://localhost:3001');
+		setSocket(s);
 
-        return () => {
-            s.disconnect()
-        }
-    }, [])
+		return () => {
+			s.disconnect();
+		};
+	}, []);
 
-    useEffect(() => {
+	useEffect(() => {
+		if (socket == null || quill == null) return;
 
-        if (socket == null || quill == null) return
+		socket.once('load-document', (document) => {
+			quill.setContents(document);
+			quill.enable();
+		});
 
-        const handler = (delta) =>{
-            quill.updateContents(delta)
-        }
-        socket.on('receive-changes', handler)
+		socket.emit('get-document', documentId);
+	}, [socket, quill, documentId]);
 
-        return () => {
-         socket.off('receive-changes', handler)
-        }
-    })
+	useEffect(() => {
+		if (socket == null || quill == null) return;
 
-        useEffect(() => {
-            if (socket == null || quill == null) return
-            const handler = (delta, oldDelta, source) =>{
-                if(source !=='user') return
-                socket.emit("send-changes", delta)
-            }
-            quill.on('text-change', handler)
+		const handler = (delta) => {
+			quill.updateContents(delta);
+		};
+		socket.on('receive-changes', handler);
 
-            return () => {
-             quill.off('text-change', handler)
-            }
-        })
+		return () => {
+			socket.off('receive-changes', handler);
+		};
+	});
 
-    const wrapperRef = useCallback(wrapper => {
-        if (wrapper == null) return
+	useEffect(() => {
+		if (socket == null || quill == null) return;
+		const handler = (delta, oldDelta, source) => {
+			if (source !== 'user') return;
+			socket.emit('send-changes', delta);
+		};
+		quill.on('text-change', handler);
 
-        wrapper.innerHTML = ""
-        const editor = document.createElement('div')
+		return () => {
+			quill.off('text-change', handler);
+		};
+	});
 
-        wrapper.append(editor)
-       const q = new Quill (editor, {theme: 'snow', modules: {toolbar:
-        TOOLBAR_OPTIONS}})
-        setQuill(q)
-    }, [])
-    return <div className="container" ref={wrapperRef}></div>
+	const wrapperRef = useCallback((wrapper) => {
+		if (wrapper == null) return;
+
+		wrapper.innerHTML = '';
+		const editor = document.createElement('div');
+
+		wrapper.append(editor);
+		const q = new Quill(editor, {
+			theme: 'snow',
+			modules: { toolbar: TOOLBAR_OPTIONS },
+		});
+		q.disable();
+		q.setText('Loading...');
+		setQuill(q);
+	}, []);
+	return <div className='container' ref={wrapperRef}></div>;
 }
